b8065ec0e06ad71f3acc1a9c071a30a178e4ac0c
GopikaGnath <gopikagnath97@gmail.com>
2024-07-03 10:02:38 +0530

    first commit

---
+++ BookStore/src/main/java/repository/MyBookRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import entity.MyBookList;
+
+
+@Repository
+public interface MyBookRepository extends JpaRepository<MyBookList,Integer>{
+
+}
+
